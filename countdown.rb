c337554633db97f75bb6af3dd75935dae5ba64ec
Alexander Frosell <sachafrosell@gmail.com>
2018-10-03 20:57:27 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -6,3 +6,11 @@ def countdown(integer)
   return "HAPPY NEW YEAR!"
 end
   
+def countdown_with_sleep(integer)
+  while integer > 0 
+    puts "#{integer} SECOND(S)!"
+    integer -= 1 
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
+end
